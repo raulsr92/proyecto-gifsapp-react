b8252b9fca2a08c6f18df5c81e067a4ba612c9d5
Raul Sanchez R. <raul.sr92@gmail.com>
2026-04-20 19:57:49 -0500

    Inicio de proyecto

---
+++ src/GifsApp.tsx
@@ -0,0 +1,20 @@
+
+
+const GifsApp = () => {
+
+  return (
+    <>
+        {/*Header*/}
+        <div className="content-center">
+            <h1>Buscador de Gifs</h1>
+            <p>Descubre y comparte el gif perfecto</p>
+        </div>
+
+        {/*Search*/}
+
+    
+    </>
+  )
+}
+
+export default GifsApp
